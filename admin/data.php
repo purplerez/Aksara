fe1854ff9cd9299a6a907078940174e6fc56bf0b
Retno Ires <fairy.edensor@gmail.com>
2024-10-28 14:15:52 +0700

    add:ukuran

--- admin/data.php
+++ admin/data.php
@@ -2,8 +2,15 @@
 
 if(isset($_POST['inputData'])){
 
-    $nama = $_POST['nama'];
-    echo $nama;
+    $nama = $_POST['nama']; // mengambil data nama yang berasal dari form 
+    $gender = $_POST['gender'];
+    $hobi = implode(',', $_POST['hobi']);
+    $ukuran = implode(',', $_POST['ukuran']);
+    
+    echo $nama.'<br/>'; // menampilkan isi dari variabel nama
+    echo 'gender'. $gender.'<br/>';
+    echo 'Hobi nya adalah '.$hobi.'<br/>';
+    echo 'Ukuran '.$ukuran.'<br/>';
 
 }
 ?>
--- admin/form.php
+++ admin/form.php
@@ -24,15 +24,24 @@
         Alamat : <textarea name="alamat" id="" cols="30" rows="10"></textarea>
         <br/>
         No Telp : <input type="number" name="no_telp" id=""><br/>
-        Gender : <input type="radio" name="gender" id="" value=""> Laki 
-                 <input type="radio" name="gender" id="" value=""> Perempuan
+        Gender : <input type="radio" name="gender" id="" value="Laki"> Laki 
+                 <input type="radio" name="gender" id="" value="Perempuan"> Perempuan
                  <br/>
-        Hobi : <input type="checkbox" name="hobi1" id="" value=""> Membaca <br/>
-        <input type="checkbox" name="hobi2" id="" value=""> berlari <br/>
-        <input type="checkbox" name="hobi3" id="" value=""> tidur <br/>
-        <input type="checkbox" name="hobi4" id="" value=""> main game <br/>
+        Hobi : <input type="checkbox" name="hobi[]" id="" value="maos"> Membaca <br/>
+        <input type="checkbox" name="hobi[]" id="" value="mlaku"> berlari <br/>
+        <input type="checkbox" name="hobi[]" id="" value="sare"> tidur <br/>
+        <input type="checkbox" name="hobi[]" id="" value="dolanan"> main game <br/>
+        <input type="checkbox" name="hobi[]" id="" value="Renang"> Berenang <br/>
+        <input type="checkbox" name="hobi[]" id="" value="Sepeda"> Bersepeda <br/>
         Foto : <input type="file" name="foto" id=""><br/>
+        UKURAN : <br/>
+        <?php 
+            for($i = 30; $i <= 45; $i++){ 
+        ?>
+        <input type="checkbox" name="ukuran[]" id="" value="<?= $i ?>"> <?= $i ?> <br/>
 
+        <?php } ?>
+        
         <input type="submit" value="Input Data" name="inputData"> 
         <!-- <button type="submit">Inputkan</button> -->
 
